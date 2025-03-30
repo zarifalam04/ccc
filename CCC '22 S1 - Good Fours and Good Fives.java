46329175ccc11c2e8daa9680a2e7229a7636c7a8
Zarif Alam <zarif.alam88@gmail.com>
2025-03-30 14:59:30 -0400

    Create CCC '22 S1 - Good Fours and Good Fives.java

---
+++ CCC '22 S1 - Good Fours and Good Fives.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        int num = readInt();
+        int numCount = 0;
+    for (int i = 0; i*4 <= num; i++) {
+        int remaining = num - (i*4);
+        if (remaining % 5 ==0) {
+            numCount++;
+        }
+    }
+    System.out.println(numCount);
+
+
+    }
+
+    static String next () throws IOException {
+        while (st == null || !st.hasMoreTokens())
+            st = new StringTokenizer(br.readLine().trim());
+        return st.nextToken();
+    }
+    static long readLong () throws IOException {
+        return Long.parseLong(next());
+    }
+    static int readInt () throws IOException {
+        return Integer.parseInt(next());
+    }
+    static double readDouble () throws IOException {
+        return Double.parseDouble(next());
+    }
+    static char readCharacter () throws IOException {
+        return next().charAt(0);
+    }
+    static String readLine () throws IOException {
+        return br.readLine().trim();
+    }
+}
