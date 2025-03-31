ad9b9582c9b78769419bba4810c45b9438f18105
Zarif Alam <zarif.alam88@gmail.com>
2025-03-31 18:04:28 -0400

    Create Sorting.java

---
+++ Sorting.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+import java.util.ArrayList;
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        int num = readInt();
+        int[] numbers = new int[num];
+        for (int i = 0; i < num; i++) {
+            numbers[i] = readInt();
+        }
+        Arrays.sort(numbers);
+        for (int j : numbers) {
+            System.out.println(j);
+        }
+    }
+
+    static String next () throws IOException {
+        while (st == null || !st.hasMoreTokens())
+            st = new StringTokenizer(br.readLine().trim());
+        return st.nextToken();
+    }
+    static long readLong () throws IOException {
+        return Long.parseLong(next());
+    }
+    static int readInt () throws IOException {
+        return Integer.parseInt(next());
+    }
+    static double readDouble () throws IOException {
+        return Double.parseDouble(next());
+    }
+    static char readCharacter () throws IOException {
+        return next().charAt(0);
+    }
+    static String readLine () throws IOException {
+        return br.readLine().trim();
+    }
+}
